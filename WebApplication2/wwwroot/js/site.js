6f0353428bb93cc5d1d46228c5fc412dce550e55
ILauro <lauroseibert@gmail.com>
2022-02-14 22:40:39 -0300

    Add project files.

---
+++ WebApplication2/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    $scope.coins = [];
+
+    $scope.getData = () => {
+        var url = 'Home/GetData';
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            success: function (result) {
+                if (result != null) {
+                    $scope.coins = result;
+                    console.log($scope.coins);
+                    alert('successfuly inserted');
+                }
+                else {
+                    alert('some error occured');
+                }
+            }
+        });
+    }
+});
